fa2a830f87d04ebb01ebec94c79174721b3325af
Zuzia <zuziazla91@gmail.com>
2024-10-02 21:36:42 +0200

    EntityFramework, DbContext, connection string,migration

---
+++ ProjectManager/Data/ProjectManagerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManager.Data.Models;
+
+namespace ProjectManager.Data
+{
+    public class ProjectManagerDbContext : DbContext
+    {
+        public ProjectManagerDbContext(DbContextOptions<ProjectManagerDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
